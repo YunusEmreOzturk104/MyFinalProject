192e54928dbfa5a367665bbd7a5e1c7c65ae71e7
YunusEmreOzturk104 <ozturkyunusemre511@gmail.com>
2021-04-03 15:15:59 +0300

    ..

--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
     // generic constraint generik kısıt
     // class: referans tip
